46ae4de7e97530762bedc68fbe7a3d8fd2aca229
Cecilia <ccchimento@gmail.com>
2024-07-04 16:24:58 -0600

    solution

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ This repo is a solution to the following prompt:
 Build a component that books a one-way or return flight for specified dates.
 
 ## Requirements
-The user can choose from two flight options: "One-way flight" and "Return flight".
-Input date fields
-The date input fields represent the departure date and return dates.
-The return date input is displayed if the "Return flight" option is chosen, hidden otherwise.
-Form validation should be done upon submission for invalid fields:
-Dates are in the past.
-Return date is before the departure date.
-Upon successful submission, a message is displayed informing the user of the selection:
-One-way flight: "You have booked a one-way flight on YYYY-MM-DD"
+The user can choose from two flight options: "One-way flight" and "Return flight". <br/><br/>
+Input date fields<br/>
+The date input fields represent the departure date and return dates.<br/>
+The return date input is displayed if the "Return flight" option is chosen, hidden otherwise.<br/><br/>
+Form validation should be done upon submission for invalid fields:<br/>
+Dates are in the past.<br/>
+Return date is before the departure date.<br/><br/>
+Upon successful submission, a message is displayed informing the user of the selection:<br/>
+One-way flight: "You have booked a one-way flight on YYYY-MM-DD"<br/>
 Return-flight "You have booked a return flight, departing on YYYY-MM-DD and returning on YYYY-MM-DD"
--- src/App.js
+++ src/App.js
@@ -1,9 +1,76 @@
+import { useState } from "react";
 import "./styles.css";
 
+const TODAY = formatDate(new Date());
+const DAY_IN_SECONDS = 24 * 60 * 60 * 1000;
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+
+  return [year, month, day].join("-");
+}
+
 export default function App() {
+  const [tripType, setTripType] = useState("one");
+  const [dDate, setDDate] = useState(
+    formatDate(new Date(Date.now() + DAY_IN_SECONDS))
+  );
+  const [rDate, setRDate] = useState(dDate);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (tripType === "one") {
+      alert(`You have booked a one-way flight on ${dDate}`);
+    } else {
+      alert(`You have booked a return flight, departing on ${dDate} and returning on ${rDate}`)
+    }
+  };
+
   return (
-    <form>
-      <input type="date" />
+    <form onSubmit={handleSubmit}>
+      <select
+        name="trip-type"
+        onChange={(e) => {
+          setTripType(e.target.value);
+        }}
+        value={tripType}
+      >
+        <option value="one">One-way Flight</option>
+        <option value="return">Return Flight</option>
+      </select>
+      <br />
+      <br />
+      <label htmlFor="one">Departure:</label> <br />
+      <input
+        id="one"
+        type="date"
+        value={dDate}
+        min={TODAY}
+        onChange={(e) => {
+          setDDate(e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      {tripType === "return" && (
+        <>
+          <label htmlFor="return">Return:</label> <br />
+          <input
+            id="return"
+            type="date"
+            value={rDate}
+            min={dDate}
+            onChange={(e) => {
+              setRDate(e.target.value);
+            }}
+          />
+          <br />
+          <br />
+        </>
+      )}
+      <button type="submit">Book Flight</button>
     </form>
   );
 }
